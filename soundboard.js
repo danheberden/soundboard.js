14d4b9eb9dd9af06f052e2cd6976a40661ff1652
Dan Heberden <danheberden@gmail.com>
2011-05-18 17:16:39 -0700

    Version 0.1 of soundboard.js - initial prototype

---
+++ soundboard.js
@@ -0,0 +1,78 @@
+/* soundboard.js v0.1 
+ * 
+ * Use one audio file with effects at different start/stop points
+ * to limit http requests. Also, it's just kinda cool.
+ *
+ * (c) 2011 Dan Heberden
+ *
+ * config object:
+ *    channels: how many channels to allocate (default 4)
+ *    url: the url of the combined sound clip
+ *    data: object with the playback data.
+ *      every key is the sound name, which should contain 
+ *      { start: theStartTimeInSeconds, end: theEndTimeInSeconds }
+ *    extend: alternate extend function to use, e.g. dojo.mixin
+ *       defaults to use jQuery or underscore
+*/
+(function( window ) {
+    var defaults = {
+            channels: 4,
+            // jQuery or underscore
+            extend: window.jQuery && window.jQuery.extend ||
+                    window._ && window._.extend ||
+                    undefined
+        },
+        sb = function( config ) {
+           return new sb.pt.init( config );
+        };
+
+    sb.pt = sb.prototype = {
+        constructor: "SoundBoard",
+        init: function( config ) {
+            var settings = this.settings = _.extend( {}, defaults, config ),
+                channels = [],
+                i = 0;
+            // create the channels for this soundboard
+            for( ; i < settings.channels; i++ ) {
+                var chan = document.createElement( 'audio' );
+                    chan.src = this.settings.url;
+                channels[i] = {
+                    element: chan,
+                    playing: false
+                }
+            }  
+            this.channels = channels;
+            return this;
+        },
+        play: function( sound ) {
+            var clip = this.settings.data[ sound ],
+                i = 0;
+            
+            if ( !clip ) {
+                return false;
+            }
+
+            // get an available channel
+            while( i < this.channels.length ) {
+                if ( !this.channels[i].playing ) {
+                  var ch = this.channels[ i++ ];
+                  ch.playing = true;
+                  ch.element.currentTime = clip.start;
+                  ch.element.play();
+                  setTimeout( function() {
+                    ch.element.pause();
+                    ch.playing = false;
+                  }, (clip.end - clip.start ) * 1000 );
+                  break; // we played, dont try anymore
+                }
+            }
+        },
+
+    };
+
+    // assign the prototype to to init's
+    sb.pt.init.prototype = sb.pt;
+
+    // assign SoundBoard to the global namespace
+    window.SoundBoard = sb;
+})( this );
